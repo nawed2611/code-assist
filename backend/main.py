e2d46187a44331a7a292862b4bc02d4c73771529
Mohammad Arshad <mdarshad1000@gmail.com>
2022-11-27 03:15:10 +0530

    Fix backend

--- backend/main.py
+++ backend/main.py
@@ -21,38 +21,42 @@ def code():
     if request.method == 'POST':
 
         data = request.json
-        data = data['transcripts']
+        print("Unprocessed data is ", data)
+        data_processed = data['transcripts']
+
+        print("Input by the user is" + data_processed)
         response = openai.Completion.create(
-        model="text-davinci-002",
-        prompt=data,
-        temperature=0,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
+            model="text-davinci-002",
+            prompt=data_processed,
+            temperature=0,
+            max_tokens=1000 ,
+            top_p=1,
+            frequency_penalty=0.2,
+            presence_penalty=0
         )
-        final_response = {"answer": response["choices"][0]["text"]}
+        final_response = {"answer": response["choices"][0]["text"].lstrip()}
+        print(final_response)
         return final_response, 200, {'Access-Control-Allow-Origin': '*'}
+    return "This is the final answer"
 
+# @cross_origin('*')
+# @app.route('/debug', methods=['GET', 'POST'])
+# def documentation():
 
-@cross_origin('*')
-@app.route('/debug', methods=['GET', 'POST'])
-def documentation():
+#     if request.method == 'POST':
 
-    if request.method == 'POST':
-
-        data = request.json
-        response = openai.Completion.create(
-        model="code-davinci-002",
-        prompt="Add documentation to the following code snippet: \n\n" + data,
-        temperature=0,
-        max_tokens=500,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0,
-        )
-        final_answer = {"answer": response["choices"][0]["text"]}
-        return final_answer, 200, {'Access-Control-Allow-Origin': '*'}
+#         data = request.json
+#         response = openai.Completion.create(
+#         model="code-davinci-002",
+#         prompt="Add documentation to the following code snippet: \n\n" + data,
+#         temperature=0,
+#         max_tokens=500,
+#         top_p=1.0,
+#         frequency_penalty=0.0,
+#         presence_penalty=0.0,
+#         )
+#         final_answer = {"answer": response["choices"][0]["text"]}
+#         return final_answer, 200, {'Access-Control-Allow-Origin': '*'}
 
 if __name__ == '__main__':
     app.run(debug=True)
